53ff7df01167a9a9052caee10569be1317224212
Paul Ryan <paul.walker.ryan@gmail.com>
2023-06-28 10:26:56 -0600

    finished commenting

--- week04codingassignment.js
+++ week04codingassignment.js
@@ -38,77 +38,78 @@ console.log ("#4-----------------------------------------")
 console.log ("to access the first element of any array you use array[0]")
 
 console.log ("#5------------------------------------------")
-let nameLengths = [];
-for (let i = 0; i < names.length; i++) {
-    nameLengths.push(names[i].length);
+const nameLengths = []; //set variable nameLengths to an empty array.
+for (let i = 0; i < names.length; i++) { //used a for loop to iterate through the names array that was created earlier.
+    nameLengths.push(names[i].length); //used .push to add to the nameLengths array. used names[i].length to add the length of each value in the names array. 
 }
 console.log (nameLengths);
+//maybe use map here? Could be cleaner than for loop?
 
 console.log("#6------------------------------------------")
-let nameSum = 0;
+let nameSum = 0; //created a variable to keep track of the sum of all the elements in the nameLenghts Array.
 for (let i = 0; i < nameLengths.length; i++) {
-    nameSum += nameLengths[i];
+    nameSum += nameLengths[i]; //for loop iterated through the namelengths array and then added each value to the nameSum variable.
 }
 console.log (`Sum of all the elements in the nameLengths array: ${nameSum}`);
 
 console.log (`#7------------------------------------------`);
-const wordCombine = (word, n) => {
-    let finalCombo = '';
-    for (let i = 0; i < n; i++) {
-        finalCombo += word;
+const wordCombine = (word, n) => { //used arrow function to create function with two parametns.
+    let finalCombo = ''; //created empty string variable 
+    for (let i = 0; i < n; i++) { //for loop to iterate through based on n value.
+        finalCombo += word; //adding word parameter to finalcombo
     }
     return finalCombo;
 }
-console.log (wordCombine('Hello', 3));
+console.log (wordCombine('Hello', 3)); //called function in console.log
 
 console.log(`#8----------------------------------------`);
-let fullName = (firstName, lastName) => `${firstName} ${lastName}`
+const fullName = (firstName, lastName) => `${firstName} ${lastName}` //used one line arrow function with template literals to combine names.
 console.log (fullName('Paul', 'Ryan'))
 
 console.log( `#9--------------------------------------`);
 
-let over100 = (array1) => {
-    let result = 0;    
-    for (let i = 0; i <array1.length; i++) {
-        result += array1[i];
+const over100 = (array1) => { //created arrow function with one parameter
+    let result = 0; // created result variable to use in for loop
+    for (let i = 0; i <array1.length; i++) { //for loop to iterate through the array
+        result += array1[i]; //adding each element of the array to the result variable. 
     }
-if (result > 100){
+if (result > 100){ //if/else statment to return true if result is greater than 100
     return true;
     } else {
         return false;
     }
 }
-console.log(over100([4, 5, 6, 9, 10]));
-console.log(over100([80, 90, 100]));
+console.log(over100([4, 5, 6, 9, 10])); //testing - expect false
+console.log(over100([80, 90, 100]));// testing - expect true
 
 console.log (`#10-------------------------------------`);
 
-let arrayAverage = (array2) => {
-    let average = 0;
+const arrayAverage = (array2) => { //arrow function to return average of elements in the array
+    let total = 0; //created total variable to store element totals from array
     for (let i = 0; i < array2.length; i++) {
-    average += array2[i];
+    total += array2[i]; //adding each element value to total
     }
-return average / array2.length;
+return total / array2.length; //dividing total variable by amount of elements in the array.
 }
 console.log (arrayAverage([1, 2, 3, 10, 10]));
 
 console.log(`#11------------------------------------`);
-let greaterAverage = (bigArray, smallArray) => {
+const greaterAverage = (firstArray, secondArray) => { //created arrow function with two parameters
     let average1 = 0;
     let average2 = 0;
-    let bigArrayAverage = 0;
-    let smallArrayAverage = 0;
+    let firstArrayAverage = 0;
+    let secondArrayAverage = 0;//created empty variables to hold values of loops
     
-    for (let i = 0; i < bigArray.length; i++) {
-    average1 += bigArray[i];
-    bigArrayAverage = average1 / bigArray.length;}
+    for (let i = 0; i < firstArray.length; i++) {
+    average1 += firstArray[i];
+    firstArrayAverage = average1 / firstArray.length;}//for loop to find average of big array
 
-    for (let i = 0; i < smallArray.length; i++) {
-    average2 += smallArray[i];
-    smallArrayAverage = average2 / smallArray.length;
-    }
+    for (let i = 0; i < secondArray.length; i++) {
+    average2 += secondArray[i];
+    secondArrayAverage = average2 / secondArray.length;
+    }//for loop to find small loop average
     
-        if (bigArrayAverage > smallArrayAverage) {
+        if (firstArrayAverage > secondArrayAverage) { //if statement to compare first array to second array.
         return true;
         } else {
         return false;
@@ -116,19 +117,33 @@ let greaterAverage = (bigArray, smallArray) => {
     }
    
 }
-console.log(greaterAverage([3,4,7,10,5], [2, 5, 6]));
-console.log(greaterAverage([2,3], [3,4,5,7,8,6,7]));
+console.log(greaterAverage([3,4,7,10,5], [2, 5, 6]));//testing expect true
+console.log(greaterAverage([2,3], [3,4,5,7,8,6,7])); //testing expect false
 
 console.log(`#12------------------------------------`);
-let willBuyDrink = (isHotOutside, moneyInPocket) => {
-    if (isHotOutside === true && moneyInPocket > 10.50) {
+const willBuyDrink = (isHotOutside, moneyInPocket) => { //created arrow function with two parameters
+    if (isHotOutside === true && moneyInPocket > 10.50) {//if statement to see if it is hot outside and money is over 10.50
         return true;
     }else {
         return false;
     }
 }
-console.log(willBuyDrink (true, 11));
-console.log(willBuyDrink(false, 12));
-console.log(willBuyDrink(true, 5));
+console.log(willBuyDrink (true, 11)); //testing expect true
+console.log(willBuyDrink(false, 12));//testing expect false
+console.log(willBuyDrink(true, 5));//testing expect false
 
 console.log (`#13--------------------------------`);
+
+//created an arrow function to determing if it was time to work out. There
+//are three parameters and the function checks to make sure you have enough time, are motivated and if you worked out yesterday. 
+const timeToWorkOut = (timeAvaliable, motivated, workedOutYesterday) => {
+    if (timeAvaliable >= 1 && motivated === true && workedOutYesterday === false) {
+        console.log ("Time to work out");
+    } else {
+        console.log("No working out today");
+    }
+}
+timeToWorkOut(2, true, false)
+timeToWorkOut(.5, true, false)
+ timeToWorkOut(2, false, false)
+timeToWorkOut(2, true, true)
